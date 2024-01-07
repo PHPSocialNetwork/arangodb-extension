3ae5f8a9fcaa1bc6a69140912176f223e3a2926e
Georges <contact@geolim4.com>
2024-01-07 13:33:21 +0100

    Simplified test config object

--- tests/Configs/github-actions.php
+++ tests/Configs/github-actions.php
@@ -2,16 +2,16 @@
 
 use Phpfastcache\Drivers\Arangodb\Config as ArangodbConfig;
 
-return (fn(ArangodbConfig $config) => $config->setItemDetailedDate(true)
+return (new ArangodbConfig())
     /*
      $config->setTraceFunction(\Closure::fromCallable(static function ($type, $data) use ($testHelper){
        $testHelper->printDebugText(sprintf('Trace for %s: %s', strtoupper($type), $data));
      }));
     */
+    ->setItemDetailedDate(true)
     ->setCollection('phpfastcache')
     ->setAuthUser('phpfastcache')
     ->setAuthPasswd('travis')
     ->setDatabase('phpfastcache')
     ->setConnectTimeout(5)
-    ->setAutoCreate(true)
-)(new ArangodbConfig());
+    ->setAutoCreate(true);
